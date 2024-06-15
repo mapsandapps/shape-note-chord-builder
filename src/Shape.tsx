8239754d54122b153a545b35e363cc7c71efa953
mapsandapps <shawn@mapsandapps.net>
2024-06-14 20:24:34 -0400

    adjust note name display

--- src/Shape.tsx
+++ src/Shape.tsx
@@ -23,40 +23,41 @@ export default function Shape(props: ShapeProps) {
   // @ts-ignore
   const noteName = getNoteName(mode, keyName, note.pitch);
 
-  const noteText = `${note.pitch}${noteName ? ` ${noteName}` : ''}`
+  const leftMargin = 24
 
   return (
     <div>
-      <svg height="64" width="80" xmlns="http://www.w3.org/2000/svg">
+      <svg height="64" width="128" xmlns="http://www.w3.org/2000/svg">
         {shape === '◺' && (
           <>
-            <polygon points="9,16 9,48 57,48" style={shapeStyle} /> 
-            {/* <path d='M 8,8 L 8,56 L 56,56 Z
-              M 16,16 L 48,48 L 16,48 Z' /> */}
-            <text x={noteText ? 16 : 16} y="42" style={{ fill: 'white' }}>{noteText}</text>
+            <text x="0" y="42">{ noteName }</text>
+            <polygon points={`${1 + leftMargin},16 ${1 + leftMargin},48 ${49 + leftMargin},48`} style={shapeStyle} /> 
+            <text x={8 + leftMargin} y="42" style={{ fill: 'white' }}>{ note.pitch }</text>
           </>
         )}
         {shape === '○︎' && (
           <>
-            <path d="M24.7377 50C51.623 50 57 30.6861 57 25.9562C57 18.5985 50.3115 14 40.8689 14C16.6066 14 9 30.5547 9 38.0438C9 45.6642 15.8197 50 24.7377 50Z" style={shapeStyle} /> 
-            <text x={noteText ? 16 : 28} y="38" style={{ fill: 'white' }}>{noteText}</text>
+            <text x="0" y="38">{ noteName }</text>
+            <path d={`M${16.7377 + leftMargin} 50C${43.623 + leftMargin} 50 ${49 + leftMargin} 30.6861 ${49 + leftMargin} 25.9562C${49 + leftMargin} 18.5985 ${42.3115 + leftMargin} 14 ${32.8689 + leftMargin} 14C${8.6066 + leftMargin} 14 ${1 + leftMargin} 30.5547 ${1 + leftMargin} 38.0438C${1 + leftMargin} 45.6642 ${7.8197 + leftMargin} 50 ${16.7377 + leftMargin} 50Z`} style={shapeStyle} /> 
+            <text x={20 + leftMargin} y="38" style={{ fill: 'white' }}>{ note.pitch }</text>
           </>
         )}
         {shape === '▭' && (
           <>
-            <polygon points="9,16 9,48 57,48 57,16" style={shapeStyle} /> 
-            <text x={noteText ? 16 : 28} y="38" style={{ fill: 'white' }}>{noteText}</text>
+            <text x="0" y="38">{ noteName }</text>
+            <polygon points={`${1 + leftMargin},16 ${1 + leftMargin},48 ${49 + leftMargin},48 ${49 + leftMargin},16`} style={shapeStyle} /> 
+            <text x={20 + leftMargin} y="38" style={{ fill: 'white' }}>{ note.pitch }</text>
           </>
         )}
         {shape === '◇' && (
           <>
-            <polygon points="32,14 56,32 32,50 8,32" style={shapeStyle} /> 
-            <text x={noteText ? 16 : 28} y="38" style={{ fill: 'white' }}>{noteText}</text>
+            <text x="0" y="38">{ noteName }</text>
+            <polygon points={`${24 + leftMargin},14 ${48 + leftMargin},32 ${24 + leftMargin},50 ${leftMargin},32`} style={shapeStyle} /> 
+            <text x={20 + leftMargin} y="38" style={{ fill: 'white' }}>{ note.pitch }</text>
           </>
         )}
-        {note.isBass && (<text x="58" y="38" style={{ fontSize: '10px' }}>bass</text>) }
+        {note.isBass && (<text x={`${50 + leftMargin}`} y="38" style={{ fontSize: '10px' }}>bass</text>) }
       </svg>
-      {/* <span>{note.isMelody && 'melody'}</span> */}
     </div>
   );
 }
