35ec5e081aecbc81c193a0e96e5913d72c0c9ec9
mapsandapps <shawn@mapsandapps.net>
2024-09-10 10:16:43 -0400

    Fix bug with logic

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
   return (
     <>
       <h1>Shape Note Chord Builder</h1>
-      <p>This is just a lil tool to help you know what chords are possible based on a melody or other note. It only includes the most common chords (i.e. there's no discords). The commonality of various chords is based on research & writing from Aldo Thomas Ceresa and <a href="https://robertkelleyphd.com/home/ShapeNoteMusic/ACorpusBasedModelOfHarmonicFunctionInShapeNoteHymnodyPaper.pdf" target="_blank">Robert Kelley</a>. <button onClick={() => showTutorial(!isShowingTutorial)}>Read more</button></p>
+      <p>This is just a lil tool to help you know what chords are possible based on a melody or other note. It only includes the most common chords (i.e. there's no discords). The commonality of various chords is based on research & writing from Aldo Thomas Ceresa and <a href="https://robertkelleyphd.com/home/ShapeNoteMusic/ACorpusBasedModelOfHarmonicFunctionInShapeNoteHymnodyPaper.pdf" target="_blank">Robert Kelley</a>. <button onClick={() => showTutorial(!isShowingTutorial)}>{isShowingTutorial ? 'Return to app' : 'Read more'}</button></p>
       {isShowingTutorial && <Tutorial />}
-      <div style={{ display: isShowingTutorial ? 'block' : 'none' }}>
+      <div style={{ display: isShowingTutorial ? 'none' : 'block' }}>
         {/* we want this here but invisible, so it won't lose its state when showing the tutorial */}
         <Main />
       </div>
